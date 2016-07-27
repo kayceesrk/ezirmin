4f0626a3dd1712dfe5a809462a3b80db8d404fbd
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2016-07-27 17:45:41 +0100

    Initial commit.

---
+++ pkg/pkg.ml
@@ -0,0 +1,9 @@
+#!/usr/bin/env ocaml
+#use "topfind"
+#require "topkg"
+open Topkg
+
+let () =
+  Pkg.describe "ezirmin" @@ fun c ->
+  Ok [ Pkg.mllib "src/ezirmin.mllib";
+       Pkg.test "test/test"; ]
