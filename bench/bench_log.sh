52ae75e83dc83f2b725134ee9ef6e87970032ad8
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2016-12-28 13:01:49 +0000

    Add usage info

--- bench/bench_log.sh
+++ bench/bench_log.sh
@@ -2,6 +2,12 @@
 
 rm -rf /tmp/ezirmin
 
+if [ -z "$1" ]
+then
+	echo "Usage: $0 <_build/bench/bench_*.native>"
+	exit 1
+fi
+
 for i in {1..25}
 do
 	{ time `for j in {1..100}; do echo "Hello, world!"; done | $1`; } 2>&1 | grep "real" | sed "s/real.*m\(.*\)s.*/\1/"
